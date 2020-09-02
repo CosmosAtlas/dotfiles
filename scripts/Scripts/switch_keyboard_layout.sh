32fd9ac7620c42f8e79d57ab47ff52337657ec9c
Wenhan Zhu (Cosmos) <zhuwenhan950913@gmail.com>
2020-09-02 15:32:18 +0800

    Added shortcut for switching keyboard layout in sxhkd for dwm and update script for switching layout to send notifications

--- scripts/Scripts/switch_keyboard_layout.sh
+++ scripts/Scripts/switch_keyboard_layout.sh
@@ -4,6 +4,8 @@ CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==4{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" == "dvp" ]; then
     setxkbmap -layout us
+    notify-send "Keyboard layout: US"
 else
     setxkbmap -layout us -variant dvp -option 'caps:ctrl_modifier'
+    notify-send "Keyboard layout: DVP"
 fi
