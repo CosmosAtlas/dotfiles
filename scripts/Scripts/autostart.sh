88b114ec55c29d789173d7ff567bda73b0b7c2c3
Wenhan Zhu (Cosmos) <zhuwenhan950913@gmail.com>
2020-11-17 22:26:40 +0800

    changed to `pkill -x` for more precision when restarting applications

--- scripts/Scripts/autostart.sh
+++ scripts/Scripts/autostart.sh
@@ -17,36 +17,36 @@ xsetroot -cursor_name left_ptr &
 # Launch polkit
 setsid /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
 
-pkill picom
+pkill -x picom
 nitrogen --restore; sleep 1; picom -b &
 
-pkill xfce4-power-manager
+pkill -x xfce4-power-manager
 xfce4-power-manager &
 
-pkill udiskie
+pkill -x udiskie
 udiskie --tray &
 
-pkill dunst
+pkill -x dunst
 dunst &
 
-pkill pasystray
+pkill -x pasystray
 pasystray &
 
 pm2 kill
 pm2 start clash
 
-pkill xidlehook
+pkill -x xidlehook
 xidlehook --not-when-audio --not-when-fullscreen --timer 600 "betterlockscreen -l blur" '' &
 
 eval $(/usr/bin/gnome-keyring-daemon --start --components=pkcs11,secrets,ssh) &
 
-pkill dwmblocks
+pkill -x dwmblocks
 dwmblocks &
 
-pkill fcitx5
+pkill -x fcitx5
 fcitx5 &
 
-pkill aria2c
+pkill -x aria2c
 aria2c -D &
 aria2c --conf-path ~/.config/aria2/win-aria2.conf -D &
 
