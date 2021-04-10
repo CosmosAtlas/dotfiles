e33ea73e4ec8138cd4e6d89a92cbff4983baa8c7
Wenhan Zhu (Cosmos) <zhuwenhan950913@gmail.com>
2021-04-10 22:22:13 +0800

    added xmonad configs

--- scripts/Scripts/xcape.sh
+++ scripts/Scripts/xcape.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Terminate already running bar instances
-killall -q xcape
+pkill -x xcape
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x xcape >/dev/null; do sleep 1; done
