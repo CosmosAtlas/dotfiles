ff57f6813c0a8370eb710699d74cd17a2a806147
Wenhan Zhu <zhuwenhan950913@gmail.com>
2022-04-20 14:39:50 -0400

    laptop settings update

--- scripts/Scripts/polylaunch.sh
+++ scripts/Scripts/polylaunch.sh
@@ -2,7 +2,6 @@
 
 killall -9 polybar
 
-echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+echo "---" | tee -a /tmp/polybar.log
 
-MONITOR=HDMI-A-0 polybar -c ~/.config/polybar/config.ini vert >> /tmp/polybar1.log 2>&1 &
-MONITOR=HDMI-A-1 polybar -c ~/.config/polybar/config.ini main >> /tmp/polybar2.log 2>&1 &
+polybar example >> /tmp/polybar.log 2>&1 &
