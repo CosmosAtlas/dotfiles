3cabb84dcedffd94bd824f83b499eb1f0373bc3d
Cosmos Zhu <zhuwenhan950913@gmail.com>
2022-05-03 14:16:44 -0400

    deligates keyboard management to keyd instead of xorg features

--- scripts/Scripts/switch_keyboard_layout.sh
+++ scripts/Scripts/switch_keyboard_layout.sh
@@ -3,7 +3,7 @@
 CURRENT_LAYOUT=$(setxkbmap -query | awk -F : 'NR==4{print $2}' | sed 's/ //g')
 
 if [ "$CURRENT_LAYOUT" == "dvp" ]; then
-    setxkbmap -layout us
+    setxkbmap -layout us -option 'caps:ctrl_modifier'
     notify-send "Keyboard layout: US"
 else
     setxkbmap -layout us -variant dvp -option 'caps:ctrl_modifier'
