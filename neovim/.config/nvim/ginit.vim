f3b4637eabd3780bad0fd1b9ae0a5b753b685179
Wenhan Zhu <zhuwenhan950913@gmail.com>
2023-02-04 16:42:45 -0500

    [nvim] windows setup update

--- neovim/.config/nvim/ginit.vim
+++ neovim/.config/nvim/ginit.vim
@@ -1,3 +1,5 @@
-Guifont Sarasa Mono SC Nerd Font:h13
+Guifont Sarasa Mono SC Nerd Font:h10
 GuiPopupmenu 0
 GuiTabline 0
+
+GuiWindowOpacity 0.9
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -38,7 +38,12 @@ require('packer').startup(function(use)
     'kyazdani42/nvim-tree.lua',
     requires = { 'kyazdani42/nvim-web-devicons' },
     config = function()
-      require'nvim-tree'.setup{}
+      require'nvim-tree'.setup{
+        update_focused_file = {
+          enable = true,
+          update_cwd = true,
+        },
+      }
     end,
   }
   use {
