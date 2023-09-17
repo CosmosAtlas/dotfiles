9e171ec08aeae127cc7737ac27d42983577738ef
Wenhan Zhu <zhuwenhan950913@gmail.com>
2023-09-17 13:06:27 -0400

    automatically change directory

--- neovim/.config/nvim/lua/plugins/colors.lua
+++ neovim/.config/nvim/lua/plugins/colors.lua
@@ -4,7 +4,7 @@ return -- colorschemes!
     'rafi/awesome-vim-colorschemes',
     priority = 1000,
     config = function()
-      vim.cmd.colorscheme('OceanicNext')
+      vim.cmd.colorscheme('OceanicNextLight')
     end
   },
   {
