a2c6ac21649571543650a9e814cc20c61a982a4a
Wenhan Zhu (Cosmos) <wzhu.cosmos@gmail.com>
2025-07-03 11:02:35 -0400

    [neovim] added formatting for python

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -1,17 +1,17 @@
-return { -- a list of misc and small extensions to make life easier
-  { 'romainl/vim-cool' }, -- auto clear highlights
+return {                      -- a list of misc and small extensions to make life easier
+  { 'romainl/vim-cool' },     -- auto clear highlights
   { 'tpope/vim-commentary' }, -- easy comments
-  { 'tpope/vim-repeat' }, -- dot repeat for plugins
+  { 'tpope/vim-repeat' },     -- dot repeat for plugins
   { 'tpope/vim-unimpaired' }, -- quick shortcuts
-  { 'tpope/vim-surround' }, -- adding brackets around code
-  { 'tpope/vim-dispatch' }, -- run jobs asynchronously
-  { 'tommcdo/vim-lion' }, -- alignment with gl commands
+  { 'tpope/vim-surround' },   -- adding brackets around code
+  { 'tpope/vim-dispatch' },   -- run jobs asynchronously
+  { 'tommcdo/vim-lion' },     -- alignment with gl commands
   { 'andymass/vim-matchup' }, -- better % matching
   {
     'jpalardy/vim-slime',
     config = function()
       vim.g.slime_target = 'tmux'
-      vim.cmd[[
+      vim.cmd [[
         let g:slime_default_config = {"socket_name": "default", "target_pane": "{last}"}
       ]]
     end
@@ -23,7 +23,28 @@ return { -- a list of misc and small extensions to make life easier
   {
     'windwp/nvim-autopairs',
     config = function()
-      require'nvim-autopairs'.setup{}
+      require 'nvim-autopairs'.setup {}
+    end
+  },
+  {
+    'stevearc/conform.nvim',
+    config = function()
+      require("conform").setup({
+        formatters_by_ft = {
+          python = { 'ruff_fix', 'ruff_format', 'ruff_organize_imports', }
+        },
+        format_on_save = {
+          timeout_ms = 500,
+          lsp_format = "fallback",
+        },
+      })
+
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        pattern = "*",
+        callback = function(args)
+          require("conform").format({ bufnr = args.buf })
+        end,
+      })
     end
   },
 }
