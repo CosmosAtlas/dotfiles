d2557d4ab6ee37bb1262e08055a401e36ea80342
Wenhan Zhu (Cosmos) <wzhu.cosmos@gmail.com>
2025-08-26 10:24:56 -0400

    [nvim] added iron.nvim

--- neovim/.config/nvim/lua/plugins/misc.lua
+++ neovim/.config/nvim/lua/plugins/misc.lua
@@ -37,19 +37,41 @@ return {                      -- a list of misc and small extensions to make lif
     end
   },
   {
-    "pappasam/nvim-repl",
-    opts = {
-      filetype_commands = {
-        python = { cmd = "uv run ipython", filetype = "python" }
-      },
-      open_window_default = "vertical split new",
-    },
-    keys = {
-      { "<LocalLeader>rs", ":Repl<CR>", mode = "n", desc = "Start Repl" },
-      { "<LocalLeader>c", "<Plug>(ReplSendCell)", mode = "n", desc = "Send Repl Cell" },
-      { "<LocalLeader>l", "<Plug>(ReplSendLine)", mode = "n", desc = "Send Repl Line" },
-      { "<LocalLeader>h", "<Plug>(ReplSendLine)", mode = "n", desc = "Send Repl Line" },
-      { "<LocalLeader>h", "<Plug>(ReplSendVisual)", mode = "x", desc = "Send Repl Selected" },
-    }
+    "Vigemus/iron.nvim",
+    config = function()
+      local iron = require("iron.core")
+      local view = require("iron.view")
+      local common = require("iron.fts.common")
+
+      iron.setup {
+        config = {
+          scratch_repl = false,
+          repl_definition = {
+            python = {
+              command = { "uv", "run", "ipython", "--no-autoindent", "--no-confirm-exit" },
+              format = common.bracketed_paste_python,
+              block_dividers = { "# %%", "#%%" },
+            }
+          },
+          repl_filetype = function(bufnr, ft)
+            return ft
+          end,
+          repl_open_cmd = view.split.vertical.rightbelow(0.382),
+        },
+        keymaps = {
+          toggle_repl = "<LocalLeader>rr",
+          restart_repl = "<LocalLeader>rR",
+          send_line = "<LocalLeader>l",
+          send_code_block = "<LocalLeader>c",
+          visual_send = "<LocalLeader>h",
+          exit = "<LocalLeader>rq",
+          clear = "<LocalLeader>rl",
+        },
+        ignore_blank_lines = true,
+      }
+
+      vim.keymap.set('n', '<Leader>rf', '<cmd>IronFocus<cr>')
+      vim.keymap.set('n', '<Leader>rh', '<cmd>IronHide<cr>')
+    end
   }
 }
